ae63f66f90cb8cb8756472f5354541d2ab4b98a7
Coding Krishna <codingkrishna@gmail.com>
2023-09-16 16:27:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,49 +7,23 @@ var { createMongoDBDataAPI } = require('mongodb-data-api');
 app.use( bodyParser.json() );
 app.use(express.static('public'));
 
-const api = createMongoDBDataAPI({
-   apiKey: 'mAoEjYd86Hlb4jvmIQLV1voMpATzcmdnDSSoS0wGYYWEWAW490Sc1sJczhHUNksY',
-   urlEndpoint: 'https://data.mongodb-api.com/app/data-anhmq/endpoint/data/v1'
- })
-
+let users = [{id:1, name:"ravi", id:2,name:"siva"}]
 app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
 
 app.post('/createUser', function (req, res) {
    // Prepare output in JSON format
-   api
-   .insertOne({
-     dataSource: 'Cluster0',
-     database: 'pc_db',
-     collection: 'pc_data',
-     document: {
-         user_id: req.body.id,
-         name: req.body.name,
-         email: req.body.email,
-         photoUrl: req.body.photoUrl
-     }
-   })
-   .then((result) => {
-     console.log(result.insertedId);
-     res.writeHead(200, {'Content-Type': 'application/json'});
-     res.end(JSON.stringify(result));
-   }).catch(err => {
-      console.log(err);
-   })
-   
+   let user = {id: req.body.id,
+         name: req.body.name}
+   users.push(user);
+   res.writeHead(200, {'Content-Type': 'application/json'});
+   res.end(JSON.stringify(user));
 })
 
 app.get('/listUsers', function (req, res) {
-   api.$$action('find', {
-      dataSource: 'Cluster0',
-      database: 'pc_db',
-      collection: 'pc_data',
-      filter: {}
-    }).then(result =>{
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify(result));
-    })
+   res.writeHead(200, {'Content-Type': 'application/json'});
+   res.end(JSON.stringify(users));
 })
 
 var server = app.listen(3000, function () {
