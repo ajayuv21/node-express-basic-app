826588fe98fae15e1be5767e340de3bcab08a829
codingkrishna <codingkrishna@gmail.com>
2023-10-18 07:04:22 +0530

    jobs added

--- server.js
+++ server.js
@@ -7,23 +7,55 @@ var { createMongoDBDataAPI } = require('mongodb-data-api');
 app.use( bodyParser.json() );
 app.use(express.static('public'));
 
-let users = [{id:1, name:"ravi", id:2,name:"siva"}]
+const api = createMongoDBDataAPI({
+   apiKey: 'lr6gxmzJ2BgOxwdfH1x0t5AHAwBSXUjUoZDcN3s3FJmVZVgZH2lpaDzKyaXerxVj',
+   urlEndpoint: 'https://ap-south-1.aws.data.mongodb-api.com/app/data-oubos/endpoint/data/v1'
+ })
+
 app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
 
-app.post('/createUser', function (req, res) {
-   // Prepare output in JSON format
-   let user = {id: req.body.id,
-         name: req.body.name}
-   users.push(user);
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   res.end(JSON.stringify(user));
+app.post('/jobs', function (req, res) {
+   api
+   .insertOne({
+     dataSource: 'pc-cluster',
+     database: 'test',
+     collection: 'jobs',
+     document: {
+         reqId: req.body.reqId,
+         title: req.body.title,
+         experience: req.body.experience,
+         location: req.body.location,
+         skills: req.body.skills,
+         jobDescription: req.body.jobDescription
+     }
+   })
+   .then((result) => {
+     console.log(result.insertedId);
+     res.writeHead(200, {'Content-Type': 'application/json'});
+     res.end(JSON.stringify(result));
+   }).catch(err => {
+      console.log(err);
+      res.writeHead(500, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(err));
+   })
 })
 
-app.get('/listUsers', function (req, res) {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   res.end(JSON.stringify(users));
+app.get('/jobs', function (req, res) {
+   api.$$action('find', {
+      dataSource: 'pc-cluster',
+      database: 'test',
+      collection: 'jobs',
+      filter: {}
+    }).then(result =>{
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(result));
+    }).catch(err => {
+      console.log(err);
+      res.writeHead(500, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(err));
+   })
 })
 
 var server = app.listen(3000, function () {
